962265c17942f54109e9f2743c4e2d6d011a562d
SergoShe <snow5373@gmail.com>
2022-04-18 22:11:26 +0700

    Binary Search from Chapter 1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
 public class Main {
+    public static int binarySearch(int[] array, int item) {
+        int low = 0;
+        int high = array.length - 1;
+        while (low<=high) {
+            int mid = (low + high)/2;
+            int guess = array[mid];
+            if (guess == item)
+                return mid;
+            else if (guess > item)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] my_list = {1, 3, 5, 7, 9};
+        System.out.println(binarySearch(my_list,3));
+        System.out.println(binarySearch(my_list,-1));
+    }
 }
