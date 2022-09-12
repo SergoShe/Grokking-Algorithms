20e6f5554defe0eabfe663696c0bc86276007ed1
SergoShe <snow5373@gmail.com>
2022-09-13 00:18:11 +0700

    some changes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Random;
-import java.util.HashSet;
 
 public class Main {
 
@@ -16,47 +15,6 @@ public class Main {
         int[] myList2 = {0, 1, 0, -1, 6, -48, 34, 1};
         //int[] myList2 = {-1, 0,1, -1, 0, -1, 0, 1};
         System.out.println(Arrays.toString(Chapter2.selectionSort(myList2)));
-        */
-        //Chapter3
-        Item photo = new Item("photo");
-        Item item1 = new Item("item1");
-        Item item2 = new Item("item2");
-        Item item3 = new Item("item3");
-        Item item4 = new Item("item4");
-        Box box3 = new Box("box3");
-        Item doll = new Item("doll");
-        Item notebook = new Item("notebook");
-        Item key = new Item("key");
-        Box box4 = new Box("box4", new HashSet<>(Arrays.asList(doll, notebook, key))); //key is here
-        Item pen = new Item("pen");
-        Item book = new Item("book");
-        Item star = new Item("star");
-        Item ball = new Item("ball");
-        Box box5 = new Box("box5", new HashSet<>(Arrays.asList(pen, book, star, ball)));
-        Box box2 = new Box("box2", new HashSet<>(Arrays.asList(box3, box4, item4, box5)));
-        Box box1 = new Box("box1", new HashSet<>(Arrays.asList(item1, item2, box2, item3)));
-        Box mainBox = new Box("mainBox", new HashSet<>(Arrays.asList(photo, box1)));
-        String itemName = "key";
-
-        long start = System.nanoTime();
-        Material item_1 = Chapter3.lookForItem_1(mainBox, itemName);
-        long finish = System.nanoTime();
-        if (item_1!=null) {
-            System.out.println(item_1.getName());
-        }
-        System.out.println(finish - start);
-
-        start = System.nanoTime();
-        Material item_2 = Chapter3.lookForItem_2(mainBox, itemName);
-        if (item_2!=null) {
-            System.out.println(item_2.getName());
-        }
-        finish = System.nanoTime();
-        System.out.println(finish - start);
-        /*
-        Chapter3.countdown(5);
-        Chapter3.greet("Maggie");
-        System.out.println(Chapter3.fact(5));
 
         //Chapter4
         int[] myList4 = new int[1000];
