0b02a094bdc0b010706cc451b5720910f9c9f7ce
SergoShe <snow5373@gmail.com>
2022-09-29 00:37:45 +0700

    description added

--- src/chapter8/Chapter8.java
+++ src/chapter8/Chapter8.java
@@ -4,6 +4,23 @@ import java.util.*;
 
 public class Chapter8 {
 
+    /**
+     * Метод findStations принимает множество statesNeeded, в котором указаны требуемые штаты для покрытия, и
+     * хеш-таблицу stations, в котором указаны станции и те штаты, которые они покрывают.
+     * В самом методе инициализируется множество finalStation - итоговый набор станций,
+     * которые будут выбраны из stations.
+     * Пока statesNeeded не будет пустой, внутри цикла while происходит перебор станций для поиска лучших станций.
+     * Для начала создаются переменные bestStation, куда будет записываться лучшая станция при переборе станций, а также
+     * statesCovered - множество штатов, которые входят в текущую станцию и не входящих в текущее покрытие.
+     * Внутри цикла for создается множество covered, куда по очереди будут записываться значения station. Далее
+     * путем пересечения множеств в covered остаются штаты, которые входят в покрытие текущей станцией.
+     * Если размер множества covered больше, чем statesCovered, то в bestStation записывается текущий ключ station, а в
+     * statesCovered - значение covered.
+     * После перебора станций, текущая лучшая станция добавляется в finalStations, а из statesNeeded удаляются элементы,
+     * которые остались в statesCovered.
+     * Метод возвращает множество finalStations.
+     */
+
     public static Set<String> findStations(Set<String> statesNeeded, HashMap<String, Set<String>> stations) {
         Set<String> finalStations = new HashSet<>();
         while (!statesNeeded.isEmpty()) {
--- src/chapter8/Main.java
+++ src/chapter8/Main.java
@@ -1,15 +1,12 @@
 package chapter8;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         //Chapter8
         Set<String> statesNeeded = new HashSet<>(Arrays.asList("mt", "wa", "or", "id", "nv", "ut", "ca", "az"));
-        HashMap<String, Set<String>> stations = new HashMap<>();
+        HashMap<String, Set<String>> stations = new LinkedHashMap<>();
         stations.put("kone",new HashSet<>(Arrays.asList("id", "nv", "ut")));
         stations.put("ktwo",new HashSet<>(Arrays.asList("wa","id","mt")));
         stations.put("kthree",new HashSet<>(Arrays.asList("or","nv","ca")));
